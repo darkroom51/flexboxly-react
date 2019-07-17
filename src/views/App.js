afcca5554e7c62f3461e8cf30040635482877cf2
darkroom51 <darkroom@wp.pl>
2019-07-17 18:11:05 +0200

    add mockup data

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.PUBLIC_URL}data.json`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({articles: data});
+      });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.articles.map(article => (
+          <div key={article.id}>{article.title}</div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
