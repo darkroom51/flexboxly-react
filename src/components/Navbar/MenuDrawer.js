3ac0b1e91268ef30dec78f84bce72c0dc503f38d
darkroom51 <darkroom@wp.pl>
2019-07-18 21:13:00 +0200

    add animation to MenuDrawer

--- src/components/Navbar/MenuDrawer.js
+++ src/components/Navbar/MenuDrawer.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import StyledIconButton from "../common/StyledIconButton";
 import Icon from "../../assets/icons/cancel.svg";
+import Logo from "./Logo";
 
 const StyledOverlay = styled.div`
   position: fixed;
@@ -11,6 +12,9 @@ const StyledOverlay = styled.div`
   right: 0;
   background-color: rgba(255, 255, 255, 0.8);
   z-index: 100;
+  transition: all 0.08s linear;
+  visibility: ${({ menuOpened }) => (menuOpened ? "visible" : "hidden")};
+  opacity: ${({ menuOpened }) => (menuOpened ? 1 : 0)};
 `;
 
 const StyledDrawer = styled.div`
@@ -18,22 +22,33 @@ const StyledDrawer = styled.div`
   top: 0;
   bottom: 0;
   left: 0;
-  width: 300px;
+  width: 70vw;
+  min-width: 300px;
+  max-width: 600px;
   padding: 20px 10px;
   background-color: rgba(255, 255, 255, 1);
   box-shadow: ${({ theme }) => theme.lightShadow};
   z-index: 101;
+  transition: transform 0.2s linear;
+  transform: translateX(${({ menuOpened }) => (menuOpened ? "0" : "-100%")});
 `;
+
+const StyledTopbar = styled.div`
+  display: flex;
+  align-items: center;
+ `;
+
+
 
 const DummyMenu = styled.ul`
   list-style: none;
-  padding:0;
-  margin:0;
-  margin-top: 5rem;
+  margin-top: 2rem;
 
   li {
+    font-family: ${({theme})=>theme.fontSans};
     font-size: 1.3em;
-    width:100%;
+    font-weight: 700;
+    width: 100%;
     margin: 5px 0;
     padding: 10px;
   }
@@ -44,25 +59,25 @@ const DummyMenu = styled.ul`
 `;
 
 const MenuDrawer = ({ menuOpened, toggleMenu }) => {
-  if (menuOpened) {
-    return (
-      <StyledOverlay>
-        <StyledDrawer>
-          <StyledIconButton onClick={toggleMenu} style={{float:'right'}}>
+  return (
+    <>
+      <StyledDrawer menuOpened={menuOpened}>
+        <StyledTopbar>
+          <StyledIconButton onClick={toggleMenu}>
             <img src={Icon} alt="Open Menu" />
           </StyledIconButton>
-          <DummyMenu>
-            <li>Home</li>
-            <li>Latest</li>
-            <li>Contact</li>
-            <li>About</li>
-          </DummyMenu>
-        </StyledDrawer>
-      </StyledOverlay>
-    );
-  }
-
-  return null;
+          <Logo />
+        </StyledTopbar>
+        <DummyMenu>
+          <li>Home</li>
+          <li>Latest</li>
+          <li>About</li>
+          <li>Contact</li>
+        </DummyMenu>
+      </StyledDrawer>
+      <StyledOverlay menuOpened={menuOpened} onClick={toggleMenu} />
+    </>
+  );
 };
 
 export default MenuDrawer;
