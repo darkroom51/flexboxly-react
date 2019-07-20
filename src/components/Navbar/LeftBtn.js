73429d95823b3fb8d6de03dac47bc98f762d622d
darkroom51 <darkroom@wp.pl>
2019-07-20 13:09:49 +0200

    add animated menu button

--- src/components/Navbar/LeftBtn.js
+++ src/components/Navbar/LeftBtn.js
@@ -1,9 +1,11 @@
 import React from "react";
-import MenuButton from "./MenuButton";
-import BackButton from "./BackButton";
+import AnimMenuButton from "./AnimMenuButton";
+// import MenuButton from "./MenuButton";
+// import BackButton from "./BackButton";
 
 const LeftBtn = ({ page }) => {
-  return page === "home" ? <MenuButton /> : <BackButton />;
+  // return page === "home" ? <MenuButton /> : <BackButton />;
+  return <AnimMenuButton page={page} />;
 };
 
 export default LeftBtn;
